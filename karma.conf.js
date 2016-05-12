f35fcbfb054eda119dde4038791b87c46a366782
Tatsuya Nakano <howdy@Tatsuyas-MacBook.local>
2016-05-12 22:05:46 +0900

    Step 3 Framework, ファイルの登録

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,10 @@
 module.exports = function(config) {
     config.set({
+        frameworks: ['jasmine'],
+        files: [
+            'app/app.js',
+            'test/*Spec.js'
+        ],
         logLevel: config.LOG_DEBUG
     })
 }
